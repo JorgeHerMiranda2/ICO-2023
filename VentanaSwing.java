f3670eeea797d02c75ad18d7ea99afeab4b9fb59
JorgeHerMiranda2 <125936878+JorgeHerMiranda2@users.noreply.github.com>
2023-05-07 23:05:36 -0600

    Add files via upload

---
+++ VentanaSwing.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class VentanaSwing extends JFrame {
+
+    private Jlabel etiqueta1;
+    private JTextField txtGrados;
+    private JButton boton;
+    private JLabel lblResultado;
+    private FlowLayout layout;
+
+    public VentanaSwing( String title) throws HeadlessException {
+        super(title);
+        this.setSize(800, 600);
+        layout = new FlowLayout();
+        this.getContentPane().setLayout(layout);
+        etiqueta1 = new JLabel("Grados C.");
+        this.getContentPane().add(etiqueta1);
+        txtGrados = new JTextField(10);
+        this.getContentPane().add(txtGrados);
+        boton = new JButton("Convertir");
+        this.getContentPane().add(boton);
+        lblResultado = new JLabel("0.0°");
+        this.getContentPane().add(lblResultado);
+        this.boton.addMouseListener(new MouseAdapter);
+
+        public void mouseClicked (MouseEvent e){
+            double centigrados = Double.parseDouble(txtGrados.getText());
+            double farenheid = (centigrados* 9.0/5.0)+32.0;
+            lblResultado.setText(farenheid + " °F");
+        }
+    });
+
+    this.addWindowListener(new WindowAdapter());
+
+    public void windowClosing(WindowEvent e){
+        JOptionPane.showMessageDialog(null, "Adios...");
+        System.exit(0);
+    }
+     this.setVisible(true);
+
+    }
